9d129fa62ae23b46a12e9bbf0942289f86e93060
Martin Lippl <martin.lippl@pueo.de>
2026-01-11 18:20:08 +0100

    Zwischenstand Abschnitt 8.1 - Firestore

--- src/lib/firebase/firestoreSeed.ts
+++ src/lib/firebase/firestoreSeed.ts
@@ -1,7 +1,8 @@
 import { initializeApp, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { readFileSync } from 'fs';
 import { getAuth } from 'firebase-admin/auth';
+import { events, users } from '../data/sampleData';
 
 const serviceAccount = JSON.parse(readFileSync('./src/lib/firebase/admin-creds.json', 'utf8'));
 
@@ -11,3 +12,47 @@ initializeApp({
 
 const db = getFirestore();
 const auth = getAuth();
+
+async function seedAuthUsers() {
+    for (const user of users) {
+        try {
+            const createdUser = await auth.createUser({
+                uid: user.uid,
+                email: user.email,
+                password: 'Pa$$w0rd',
+                displayName: user.displayName,
+                photoURL: user.photoURL
+            });
+
+            console.log('User created: ', createdUser.uid);
+
+            await db.collection('profiles').doc(createdUser.uid).set({
+                displayName: user.displayName,
+                photoUrl: user.photoURL,
+                email: user.email,
+                createdAt: Timestamp.now()
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+async function seedEvents() {
+    const batch = db.batch();
+    events.forEach(event => {
+        const eventRef = db.collection('events').doc(event.id);
+        batch.set(eventRef, {
+            ...event,
+            date: Timestamp.fromDate(new Date(event.date))
+        });
+    });
+    batch.commit();
+    console.log('Events seeded successfully')
+}
+
+(async () => {
+    await seedAuthUsers();
+    await seedEvents();
+    console.log('Seeded successfully')
+})()
